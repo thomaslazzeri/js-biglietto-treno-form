048ff8490666734568ee7a65977076982faeed06
thomaslazzeri3 <thomaslazzeri3@gmail.com>
2026-05-29 19:28:21 +0200

    add variables in js chilometri, anni, btnInvia

---
+++ main.js
@@ -0,0 +1,7 @@
+const chilometri = document.getElementById('chilometri');
+const anni = document.getElementById('anni');
+const btnInvia = document.getElementById('btnInvia');
+
+btnInvia.addEventListener('click', function) {
+    
+}
