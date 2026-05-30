a9f9fec09a099dd9f3ad14cbb1db2c1d61c71d8a
thomaslazzeri3 <thomaslazzeri3@gmail.com>
2026-05-30 17:04:26 +0200

    done milestone 2 changed js, add variables formBiglietto, recapChilometri and prezzoEsposto, changed btnInvia to formBiglietto, changed 'click' to 'submit', add event with comment and add textContent for recapChilometri and prezzoEsposto

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-const btnInvia = document.getElementById('btnInvia');
+const formBiglietto = document.getElementById('formBiglietto');
+const recapChilometri = document.getElementById('recapChilometri');
+const prezzoEsposto = document.getElementById('prezzoEsposto');
 
-btnInvia.addEventListener('click', function () {
+formBiglietto.addEventListener('submit', function (event) {
+    event.preventDefault(); // event fa si che non si ricarichi la pagina del browser
 
     const chilometri = parseFloat(document.getElementById('chilometri').value); // value serve a prendere cosa c'è all'interno, il numero all'interno dei chilometri, quello che digita l'utente
     const anni = parseInt(document.getElementById('anni').value); // value serve a prendere cosa c'è all'interno, il numero all'interno degli anni, quello che digita l'utente
@@ -17,6 +20,7 @@ btnInvia.addEventListener('click', function () {
     else if (anni >= 65) {
         prezzotot = prezzotot - (prezzotot * scontoOver65);
     }
-    console.log(prezzotot.toFixed(2));
+    recapChilometri.textContent = chilometri;
+    prezzoEsposto.textContent = prezzotot.toFixed(2);
 });
 
