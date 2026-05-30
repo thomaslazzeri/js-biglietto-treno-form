298692ac607f762469114c2bda1efadc1bc75316
thomaslazzeri3 <thomaslazzeri3@gmail.com>
2026-05-30 15:37:57 +0200

    add comment for value

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const btnInvia = document.getElementById('btnInvia');
 
 btnInvia.addEventListener('click', function () {
 
-    const chilometri = parseFloat(document.getElementById('chilometri').value);
-    const anni = parseInt(document.getElementById('anni').value);
+    const chilometri = parseFloat(document.getElementById('chilometri').value); // value serve a prendere cosa c'è all'interno, il numero all'interno dei chilometri, quello che digita l'utente
+    const anni = parseInt(document.getElementById('anni').value); // value serve a prendere cosa c'è all'interno, il numero all'interno degli anni, quello che digita l'utente
 
     const prezzoChilometro = 0.21;
     let prezzotot = chilometri * prezzoChilometro;
@@ -19,3 +19,4 @@ btnInvia.addEventListener('click', function () {
     }
     console.log(prezzotot.toFixed(2));
 });
+
