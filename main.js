8a312c5b01a28a58cb8fee6b981075d031be3eb4
thomaslazzeri3 <thomaslazzeri3@gmail.com>
2026-05-30 13:01:48 +0200

    add divs in html, add variables in js and changed css

--- main.js
+++ main.js
@@ -2,6 +2,12 @@ const chilometri = document.getElementById('chilometri');
 const anni = document.getElementById('anni');
 const btnInvia = document.getElementById('btnInvia');
 
+const prezzoChilometro = 0.21;
+let prezzotot = chilometri * prezzoChilometro;
+
+const scontoMinorenni = 0.20;
+const scontoOver65 = 0.40;
+
 btnInvia.addEventListener('click', function) {
-    
+   console.log(btnInvia);
 }
